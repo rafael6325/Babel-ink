e08a4cb96ad55e8eb2b10efee0234ba2b78eb360
rafa <rafael6325@outlook.com.br>
2024-07-10 00:00:54 -0300

    adicionando novos botões e começando a criar o sistema de posts

---
+++ scripts/welcome.js
@@ -0,0 +1,12 @@
+async function createPost() {
+    const post = document.getElementById('postText').value;
+    const postContainer = document.getElementById('mainPosts')
+
+    console.log(post);
+    console.log(postContainer);
+
+    // document.getElementById('rua').value = endereco.logradouro;
+    // document.getElementById('bairro').value = endereco.bairro;
+    // document.getElementById('cidade').value = endereco.localidade;
+    // document.getElementById('uf').value = endereco.uf;
+  }
